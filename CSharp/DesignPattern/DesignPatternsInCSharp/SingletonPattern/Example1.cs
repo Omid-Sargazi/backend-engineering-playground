a83396521600d9913794f6f035b8e796887647c4
Omid-Sargazi <om.sargazi@gmail.com>
2026-01-31 13:56:44 +0330

    Singleton Pattern By C#

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/SingletonPattern/Example1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.SingletonPattern
+{
+    public class Example1
+    {
+        private static Example1 _Instance;
+        private Example1()
+        {
+
+        }
+
+        public static Example1 CreateInstance()
+        {
+            if (_Instance == null) 
+            {
+                return new Example1(); 
+            }
+            return _Instance;
+        }
+    }
+
+    public class CreateInstance
+    {
+        public void Execute()
+        {
+            var obj = Example1.CreateInstance();
+        }
+    }
+}
+
