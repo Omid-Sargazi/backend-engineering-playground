0f39ac8b26d9589c677acbea7c53f863b1c022c7
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-04 11:36:49 +0330

    add Singleton Logger class with Lazy thread-safe implementation

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/SingletonPattern/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.SingletonPattern
+{
+    public class Logger
+    {
+        private static readonly Lazy<Logger> _instance = new Lazy<Logger>(() => new Logger());
+
+        private Logger()
+        {
+        }
+
+        public static Logger Instance => _instance.Value;
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+        }
+    }
+
+}
