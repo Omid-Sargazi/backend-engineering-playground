a406b0326d1ea4b1c7635e1228302241ea3bac09
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-04 11:40:39 +0330

    add thread-safe Singleton DatabaseConnection with mock query method

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/SingletonPattern/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.SingletonPattern
+{
+    public class DatabaseConnection
+    {
+        private static readonly Lazy<DatabaseConnection> _instance = new Lazy<DatabaseConnection>(() => new DatabaseConnection());
+
+        private DatabaseConnection()
+        {
+            Console.WriteLine("Database connection established.");
+        }
+
+        public static DatabaseConnection Instance => _instance.Value;
+
+        public void Query(string sql)
+        {
+            Console.WriteLine($"Executing SQL: {sql}");
+        }
+    }
+
+}
