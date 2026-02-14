16ea767f5f14cba311804c4e95851b284a421547
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-14 08:51:47 +0330

    Old logger and new with

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/AdapterPattern/Logger/ILogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.AdapterPattern.Logger
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class LegacyLogger
+    {
+        public void WriteLogToFile(string msg)
+        {
+            Console.WriteLine($"Legacy Log: {msg}");
+        }
+    }
+
+    public class LoggerAdapter : ILogger
+    {
+        private readonly LegacyLogger logger;
+        public LoggerAdapter(LegacyLogger legacyLogger)
+        {
+            logger = legacyLogger;
+        }
+        public void Log(string message)
+        {
+            logger.WriteLogToFile(message);
+        }
+    }
+}
