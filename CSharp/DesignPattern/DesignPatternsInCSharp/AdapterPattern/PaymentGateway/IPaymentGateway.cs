5abf1a1f4408d387ead80d908d8b51a0c0d0abfd
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-14 08:58:49 +0330

    Payment GateWay by Adapter pattern

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/AdapterPattern/PaymentGateway/IPaymentGateway.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.AdapterPattern.PaymentGateway
+{
+    public interface IPaymentGateway
+    {
+        public void Pay(decimal amount);
+    }
+
+    public class StripeService
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine("Stripe Payment Done");
+        }
+    }
+
+    public class StripeAdapter : IPaymentGateway
+    {
+        private readonly StripeService _service;
+        public StripeAdapter(StripeService service)
+        {
+            _service = service;
+        }
+        public void Pay(decimal amount)
+        {
+            _service.ProcessPayment(amount);
+        }
+    }
+}
