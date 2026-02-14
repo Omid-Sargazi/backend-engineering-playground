0ea3ecf89e6918f82c6e327c0fdf753a425105ab
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-14 10:41:56 +0330

    Payment Service Without Pattern

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/StrategyPattern/WithoutPattern/PaymentService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.StrategyPattern.WithoutPattern
+{
+    public class PaymentService
+    {
+        public void Pay(string type)
+        {
+            if(type == "CreditCard")
+            {
+                Console.WriteLine("Paid by Credit Card");
+            }
+            else if(type=="P[aypal")
+            {
+                Console.WriteLine("Paid by Paypal");
+            }
+            else if(type == "Crypto")
+            {
+                Console.WriteLine("Paid by Crypto");
+            }
+        }
+    }
+}
