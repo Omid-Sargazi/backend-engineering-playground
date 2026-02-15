384134ecefe5d4a956e6a5db29daf0a7874786af
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-15 11:40:37 +0330

    Bridge Pattern of Concrete Implementations

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/IPrinter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern
+{
+    public interface IPrinter
+    {
+        void Print(string context);
+    }
+
+    public class PCPrinter : IPrinter
+    {
+        public void Print(string context)
+        {
+            Console.WriteLine($"Printing from PC: {content}");
+        }
+    }
+
+    public class AndroidPrinter : IPrinter
+    {
+        public void Print(string context)
+        {
+            Console.WriteLine($"Printing from Android: {content}");
+        }
+    }
+}
