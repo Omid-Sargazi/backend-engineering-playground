a457a55487d4603a45f5e393000e9ec480c138e8
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-16 07:50:16 +0330

    Implementor

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/PaymentSystem/IPaymentGateway.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern.PaymentSystem
+{
+    public interface IPaymentGateway
+    {
+        void ProcessPayment(decimal amount);
+    }
+}
