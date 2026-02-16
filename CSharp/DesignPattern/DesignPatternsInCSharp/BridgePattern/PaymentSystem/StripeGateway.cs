b909213cd6889274f7f26297dfb0947e1b495ed3
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-16 07:51:32 +0330

    Concrete Implementations

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/PaymentSystem/StripeGateway.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern.PaymentSystem
+{
+    public class StripeGateway : IPaymentGateway
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing {amount} via Stripe");
+        }
+    }
+
+    public class ZarinpalGateway : IPaymentGateway
+    {
+        public void ProcessPayment(decimal amount)
+        {
+            Console.WriteLine($"Processing {amount} via Zarinpal");
+        }
+    }
+}
