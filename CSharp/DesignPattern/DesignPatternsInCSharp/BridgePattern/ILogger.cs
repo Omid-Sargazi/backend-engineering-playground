45e6ad4355d7326af997bc0cace80869c456fb18
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-16 07:34:35 +0330

    Refined Abstraction

--- CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/ILogger.cs
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/ILogger.cs
@@ -37,4 +37,18 @@ namespace DesignPatternsInCSharp.BridgePattern
 
         public abstract void Execute();
     }
+
+    public class PaymentService : ApplicationService
+    {
+        public PaymentService(ILogger logger) : base(logger)
+        {
+        }
+
+        public override void Execute()
+        {
+            _logger.Log("Executing payment service...");
+            // Payment logic here
+            _logger.Log("Payment executed successfully.");
+        }
+    }
 }
