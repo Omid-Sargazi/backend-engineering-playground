b7d29287b0d2148424829ec64095472e67051b28
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-16 07:33:00 +0330

    Implementor Interface and Concrete Implementors In Bridge Pattern

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/ILogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern
+{
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message) => Console.WriteLine($"Console Log: {message}");
+    }
+
+    public class FileLogger : ILogger
+    {
+        public FileLogger()
+        {
+        }
+
+        public void Log(string message) => File.AppendAllText("log.txt", $"File Log: {message}\n");
+    }
+}
