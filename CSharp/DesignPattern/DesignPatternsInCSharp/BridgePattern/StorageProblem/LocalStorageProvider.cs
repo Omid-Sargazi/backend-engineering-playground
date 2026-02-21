2f4665f888719f6f9267f918c383ed190d2826a9
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-21 07:33:21 +0330

    add LocalStorageProvider and S3StorageProvider implementations

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/StorageProblem/LocalStorageProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern.StorageProblem
+{
+    public class LocalStorageProvider : IStorageProvider
+    {
+        public void Upload(string fileName, byte[] data)
+        {
+            Console.WriteLine($"Saving {fileName} to local disk");
+        }
+    }
+}
---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/StorageProblem/S3StorageProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern.StorageProblem
+{
+    public class S3StorageProvider : IStorageProvider
+    {
+        public void Upload(string fileName, byte[] data)
+        {
+            Console.WriteLine($"Uploading {fileName} to AWS S3");
+        }
+    }
+}
