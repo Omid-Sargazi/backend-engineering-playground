4a3995f6d85b43a1b18bd413adb3dba4ec248dd6
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-21 07:35:00 +0330

    create FileResource abstraction bridging to storage provider

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/StorageProblem/FileResource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern.StorageProblem
+{
+    public abstract class FileResource
+    {
+        protected IStorageProvider _storageProvider;
+        protected FileResource(IStorageProvider storageProvider)
+        {
+            _storageProvider = storageProvider;
+        }
+
+        public abstract void Save(string fileName, byte[] data);
+    }
+}
