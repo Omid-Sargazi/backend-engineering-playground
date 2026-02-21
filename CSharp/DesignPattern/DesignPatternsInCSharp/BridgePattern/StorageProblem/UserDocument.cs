4079447870e1940be1e729e75989779de6abedf7
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-21 07:36:16 +0330

    implement UserDocument and InvoiceFile as refined abstractions

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/StorageProblem/UserDocument.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern.StorageProblem
+{
+    public class UserDocument : FileResource
+    {
+        public UserDocument(IStorageProvider storageProvider) : base(storageProvider)
+        {
+        }
+        public override void Save(string fileName, byte[] data)
+        {
+            _storageProvider.Upload(fileName, data);
+        }
+    }
+
+    public class InvoiceFile : FileResource
+    {
+        public InvoiceFile(IStorageProvider storage)
+        : base(storage) { }
+
+        public override void Save(string fileName, byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
