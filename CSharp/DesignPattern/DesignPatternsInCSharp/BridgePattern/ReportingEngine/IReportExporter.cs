fb09245ec75dce524ea3865a021d3641f81f7681
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-22 07:26:09 +0330

    introduce IReportExporter abstraction for pluggable export formats

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/BridgePattern/ReportingEngine/IReportExporter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.BridgePattern.ReportingEngine
+{
+    public interface IReportExporter
+    {
+        void Export(string reportData);
+    }
+}
