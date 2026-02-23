600bfb85bfc54c62fe3f2351cd9ccd6498d72fb4
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-23 07:37:15 +0330

    implement SalesReport and InventoryReport refined abstractions

---
+++ CSharp/LINQLab/ReportingApi/Reports/InventoryReport.cs
@@ -0,0 +1,15 @@
+﻿using ReportingApi.Abstractions;
+
+namespace ReportingApi.Reports
+{
+    public class InventoryReport : Report
+    {
+        public InventoryReport(IReportExporter exporter)
+        : base(exporter) { }
+        public override string Generate()
+        {
+            var data = "Inventory Report Data";
+            return _reportExporter.Export(data);
+        }
+    }
+}
---
+++ CSharp/LINQLab/ReportingApi/Reports/SalesReport.cs
@@ -0,0 +1,15 @@
+﻿using ReportingApi.Abstractions;
+
+namespace ReportingApi.Reports
+{
+    public class SalesReport : Report
+    {
+        public SalesReport(IReportExporter exporter)
+        : base(exporter) { }
+        public override string Generate()
+        {
+            var data = "Sales Report Data";
+            return _reportExporter.Export(data);
+        }
+    }
+}
