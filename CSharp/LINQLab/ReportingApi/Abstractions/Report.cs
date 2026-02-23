4dbe2d0559e5a247ef416a1ab4445d7c47298577
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-23 07:34:51 +0330

    create abstract Report class bridging to IReportExporter

---
+++ CSharp/LINQLab/ReportingApi/Abstractions/Report.cs
@@ -0,0 +1,13 @@
+﻿namespace ReportingApi.Abstractions
+{
+    public abstract class Report
+    {
+        protected readonly IReportExporter _reportExporter;
+        protected Report(IReportExporter reportExporter)
+        {
+            _reportExporter = reportExporter;
+        }
+
+        public abstract string Generate();
+    }
+}
