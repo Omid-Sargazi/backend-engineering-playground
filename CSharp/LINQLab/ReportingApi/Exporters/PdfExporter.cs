fa73cc86fbd6a836f5cfa76e3848965a6e8759db
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-23 07:33:20 +0330

    implement PdfExporter and ExcelExporter concrete classes

---
+++ CSharp/LINQLab/ReportingApi/Exporters/PdfExporter.cs
@@ -0,0 +1,20 @@
+﻿using ReportingApi.Abstractions;
+
+namespace ReportingApi.Exporters
+{
+    public class PdfExporter : IReportExporter
+    {
+        public string Export(string reportData)
+        {
+            return $"PDF Export: {reportData}";
+        }
+    }
+
+    public class ExcelExporter : IReportExporter
+    {
+        public string Export(string reportData)
+        {
+            return $"Excel Export: {reportData}";
+        }
+    }
+}
