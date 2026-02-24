cf58feb89813e015a97de3a799f309b3958980e6
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-24 07:48:38 +0330

    introduce IReportFactory interface to encapsulate report creation logic

--- CSharp/LINQLab/ReportingApi/Abstractions/IReportExporter.cs
+++ CSharp/LINQLab/ReportingApi/Abstractions/IReportExporter.cs
@@ -3,6 +3,6 @@
     public interface IReportExporter
     {
         string Export(string reportData);
-        string Export(string reportData);
+       
     }
 }
---
+++ CSharp/LINQLab/ReportingApi/Factories/ReportFactory.cs
@@ -0,0 +1,31 @@
+﻿using ReportingApi.Abstractions;
+using ReportingApi.Reports;
+
+namespace ReportingApi.Factories
+{
+    public class ReportFactory : IReportFactory
+    {
+        private readonly IEnumerable<IReportExporter> _exporters;
+
+        public ReportFactory(IEnumerable<IReportExporter> exporters)
+        {
+            _exporters = exporters;
+        }
+
+        public Report Create(string reportType, string format)
+        {
+            var exporter = _exporters
+                .FirstOrDefault(e => e.Format.Equals(format, StringComparison.OrdinalIgnoreCase));
+
+            if (exporter == null)
+                throw new Exception("Invalid export format");
+
+            return reportType.ToLower() switch
+            {
+                "sales" => new SalesReport(exporter),
+                "inventory" => new InventoryReport(exporter),
+                _ => throw new Exception("Invalid report type")
+            };
+        }
+    }
+}
