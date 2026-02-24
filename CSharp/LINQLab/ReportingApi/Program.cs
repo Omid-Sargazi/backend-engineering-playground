826d95fa682ebc91673fab6038e1dce649dbca58
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-24 07:41:54 +0330

    register IReportExporter implementations using interface binding

--- CSharp/LINQLab/ReportingApi/Program.cs
+++ CSharp/LINQLab/ReportingApi/Program.cs
@@ -1,3 +1,4 @@
+using ReportingApi.Abstractions;
 using ReportingApi.Exporters;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<PdfExporter>();
 builder.Services.AddScoped<ExcelExporter>();
+builder.Services.AddScoped<IReportExporter, PdfExporter>();
+builder.Services.AddScoped<IReportExporter, ExcelExporter>();
 
 var app = builder.Build();
 
