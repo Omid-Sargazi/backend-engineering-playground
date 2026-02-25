9c70790da7046a0c27b5ef117520a6f3344387f7
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-25 08:46:19 +0330

    feat: introduce discount and tax strategy interfaces for composition-based design

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/Composition/IDiscountStrategy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition
+{
+    public interface IDiscountStrategy
+    {
+        decimal ApplyDiscount(decimal amount);
+    }
+
+    public interface ITaxStrategy
+    {
+        decimal ApplyTax(decimal amount);
+    }
+}
