de84b2fd562ace00993addbc11a89b143eaeb591
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-25 09:12:57 +0330

    introduce IShippingStrategy interface for shipping behavior composition

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/Composition/AmazonOrder/IShippingStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition.AmazonOrder
+{
+    public interface IShippingStrategy
+    {
+        decimal CalculateShippingCost();
+        string Ship();
+    }
+}
