7ca93f072811278ab6f248ef7583741a5e1fc0e0
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-25 09:14:02 +0330

    feat: implement CreditCardPayment and PayPalPayment strategies

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/Composition/AmazonOrder/CreditCardPayment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition.AmazonOrder
+{
+    public class CreditCardPayment:IPaymentStrategy
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount} using Credit Card.");
+        }
+    }
+
+    public class PayPalPayment : IPaymentStrategy
+    {
+        public void Pay(decimal amount)
+        {
+            Console.WriteLine($"Paid {amount} using PayPal.");
+        }
+    }
+}
