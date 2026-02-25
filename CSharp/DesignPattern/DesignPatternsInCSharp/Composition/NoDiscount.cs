16d92c17f31a5410476d4644d1c775faeb716b4e
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-25 08:49:14 +0330

    implement concrete discount and tax strategies

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/Composition/NoDiscount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition
+{
+    public class NoDiscount : IDiscountStrategy
+    {
+        public decimal ApplyDiscount(decimal amount)
+        {
+            return amount;
+        }
+    }
+
+    public class StandardTax : ITaxStrategy
+    {
+        public decimal ApplyTax(decimal amount) => amount * 1.2m;
+    }
+}
