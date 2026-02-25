1b12a54e9dbfbf62354cc923769f8c8fc831e6d2
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-25 09:14:32 +0330

    feat: implement StandardShipping and ExpressShipping strategies

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/Composition/AmazonOrder/StandardShipping.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition.AmazonOrder
+{
+    public class StandardShipping : IShippingStrategy
+    {
+        public decimal CalculateShippingCost() => 10;
+
+        public string Ship() => "Shipped via Standard Shipping.";
+    }
+
+    public class ExpressShipping : IShippingStrategy
+    {
+        public decimal CalculateShippingCost() => 25;
+
+        public string Ship() => "Shipped via Express Shipping.";
+    }
+}
