f484243b513991abaed5b1cdceff4999c23ec8f3
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-25 09:14:46 +0330

    feat: implement RegularPricing and DiscountPricing strategies

--- CSharp/DesignPattern/DesignPatternsInCSharp/Composition/AmazonOrder/StandardShipping.cs
+++ CSharp/DesignPattern/DesignPatternsInCSharp/Composition/AmazonOrder/StandardShipping.cs
@@ -19,4 +19,14 @@ namespace DesignPatternsInCSharp.Composition.AmazonOrder
 
         public string Ship() => "Shipped via Express Shipping.";
     }
+
+    public class RegularPricing : IPricingStrategy
+    {
+        public decimal CalculatePrice(decimal basePrice) => basePrice;
+    }
+
+    public class DiscountPricing : IPricingStrategy
+    {
+        public decimal CalculatePrice(decimal basePrice) => basePrice * 0.9m;
+    }
 }
