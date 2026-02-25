a996d1b5addf367afdf2cbd7aa9da68a9b259d6a
Omid-Sargazi <om.sargazi@gmail.com>
2026-02-25 08:50:22 +0330

    feat: implement Payment class using composition for tax and discount behaviors

---
+++ CSharp/DesignPattern/DesignPatternsInCSharp/Composition/Payment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsInCSharp.Composition
+{
+    class Payment
+    {
+        private readonly IDiscountStrategy discount;
+        private readonly ITaxStrategy _tax;
+
+        public Payment(IDiscountStrategy discount, ITaxStrategy tax)
+        {
+            _discount = discount;
+            _tax = tax;
+        }
+
+        public decimal Process(decimal amount)
+        {
+            var discounted = _discount.ApplyDiscount(amount);
+            var finalAmount = _tax.ApplyTax(discounted);
+
+            return finalAmount;
+        }
+    }
+}
