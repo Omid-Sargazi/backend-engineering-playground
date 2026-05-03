650f8f75a2174780b5c2de0693c791070dc07817
Omid-Sargazi <om.sargazi@gmail.com>
2026-05-03 14:56:20 +0330

    AddTwo Nums

---
+++ CSharp/LINQLab/AlgorithemProblems/Algorithems/AddTwoNums/AddTwoNums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithems.AddTwoNums
+{
+    public class AddTwoNums
+    {
+        public int[] Execute(int[] args, int target)
+        {
+            int left = 0; int right = args.Length;
+            for(int i = left; i<=right;i++)
+            {
+                if (args[left] + args[right]==target)
+                {
+                    return [left, right];
+                }
+                left++;
+                right--;
+            }
+            return [0,0];
+        }
+    }
+}
