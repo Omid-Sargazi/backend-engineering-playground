3ede55da336a32d8dcf5c24a5c93f08f4e8bf5bf
Omid-Sargazi <om.sargazi@gmail.com>
2026-05-04 09:51:04 +0330

    Simple API Todo for create and Get

---
+++ APIProjects/API_Projects/Application/Todos/Dtos/TodoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API_Projects.Application.Todos.Dtos
+{
+    public class TodoDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime? DueTime { get; set; }
+    }
+}
---
+++ APIProjects/API_Projects/Controllers/Todo/TodoController.cs
@@ -0,0 +1,39 @@
+﻿using API_Projects.Application.Todos.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Projects.Controllers.Todo
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoController:ControllerBase
+    {
+        private static readonly List<TodoDto> _todoDtos = [];
+
+        [HttpGet]
+        public ActionResult<List<TodoDto>> GetTodos()
+        {
+            if(_todoDtos.Any())
+                return Ok(_todoDtos);
+            return NotFound("There isn`t any todos for today.");
+        }
+
+        [HttpPost]
+        public ActionResult CreateTodo([FromBody]  TodoDto dto)
+        {
+            _todoDtos.Add(dto);
+            return Ok(dto);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<TodoDto> GetTodoById(int id)
+        {
+            var result = _todoDtos.FirstOrDefault(x => x.Id == id);
+            if(result ==null)
+            {
+                return NotFound($"there is not any todo by id:{id}");
+            }
+
+            return Ok(result);
+        }
+    }
+}
