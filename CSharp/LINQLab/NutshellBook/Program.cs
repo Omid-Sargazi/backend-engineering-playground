ab82af1e58fe7bc4d6ea2cb11c3191a5f4e8b5cb
Omid-Sargazi <om.sargazi@gmail.com>
2026-06-15 10:23:28 +0330

    .

--- CSharp/LINQLab/NutshellBook/Program.cs
+++ CSharp/LINQLab/NutshellBook/Program.cs
@@ -1,71 +1,85 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using NutshellBook;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
-Task t1 = Task.Run(() =>
-{
-    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-    Console.WriteLine(Thread.CurrentThread.Name);
-});
+//Task t1 = Task.Run(() =>
+//{
+//    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+//    Console.WriteLine(Thread.CurrentThread.Name);
+//});
 
-Task t2 = Task.Run(() =>
-{
-    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-    Console.WriteLine(Thread.CurrentThread.Name);
-});
-Thread t = new Thread(WriteY);
-t.Start();
+//Task t2 = Task.Run(() =>
+//{
+//    Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+//    Console.WriteLine(Thread.CurrentThread.Name);
+//});
+//Thread t = new Thread(WriteY);
+//t.Start();
 
-Thread t3 = new Thread(Go);
-t3.Start();
-t3.Join();
-Console.Write("Thread t has ended.");
- void Go()
+//Thread t3 = new Thread(Go);
+//t3.Start();
+//t3.Join();
+//Console.Write("Thread t has ended.");
+// void Go()
+//{
+//    for(int i = 0; i < 1000; i++)
+//    {
+//        Console.Write("t3"); 
+//    }
+//}
+
+//for(int  i = 0; i < 100; i++)
+//{
+//    Console.Write("x");
+//}
+
+//void WriteY()
+//{
+//    for(int i = 0;i < 100;i++) Console.Write("Y");
+//}
+
+//bool blocked = (t3.ThreadState & ThreadState.WaitSleepJoin) != 0;
+
+//Thread_Yield  ty = new Thread_Yield();
+//ty.Run();
+
+
+//Thread dbThread = new Thread(GetBankTransactions);
+//dbThread.Start();
+
+
+//for (int i = 1; i <= 5; i++)
+//{
+//    Console.WriteLine($"Main thread doing other work... {i}");
+//    Thread.Sleep(300); // شبیه‌سازی کار دیگر
+//}
+
+//void GetBankTransactions()
+//{
+//    Console.WriteLine($"DB thread {Thread.CurrentThread.ManagedThreadId} started");
+
+//    Thread.Sleep(30);
+//    Console.WriteLine($"DB thread {Thread.CurrentThread.ManagedThreadId} - got 1,000,000 transactions");
+//}
+
+//long sum = 0;
+//for (int i = 0; i < 1000000; i++)
+//{
+//    sum += i; // شبیه‌سازی پردازش
+//}
+//Console.WriteLine($"Processing complete. Sum: {sum}");
+
+Console.WriteLine("6/15/2026");
+
+
+new Thread(GO).Start();
+GO();
+
+void GO()
 {
-    for(int i = 0; i < 1000; i++)
+    for(int cycles = 0;cycles<5;cycles++)
     {
-        Console.Write("t3"); 
+        Console.Write("?");
     }
 }
-
-for(int  i = 0; i < 100; i++)
-{
-    Console.Write("x");
-}
-
-void WriteY()
-{
-    for(int i = 0;i < 100;i++) Console.Write("Y");
-}
-
-bool blocked = (t3.ThreadState & ThreadState.WaitSleepJoin) != 0;
-
-Thread_Yield  ty = new Thread_Yield();
-ty.Run();
-
-
-Thread dbThread = new Thread(GetBankTransactions);
-dbThread.Start();
-
-
-for (int i = 1; i <= 5; i++)
-{
-    Console.WriteLine($"Main thread doing other work... {i}");
-    Thread.Sleep(300); // شبیه‌سازی کار دیگر
-}
-
-void GetBankTransactions()
-{
-    Console.WriteLine($"DB thread {Thread.CurrentThread.ManagedThreadId} started");
-
-    Thread.Sleep(30);
-    Console.WriteLine($"DB thread {Thread.CurrentThread.ManagedThreadId} - got 1,000,000 transactions");
-}
-
-long sum = 0;
-for (int i = 0; i < 1000000; i++)
-{
-    sum += i; // شبیه‌سازی پردازش
-}
-Console.WriteLine($"Processing complete. Sum: {sum}");
